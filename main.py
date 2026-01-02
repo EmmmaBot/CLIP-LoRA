5d76aa024646e2d629d123c629e31ae54b7c424d
Emma Park <tripwithsoo@gmail.com>
2026-01-02 17:33:46 +0900

    Log dataset sizes and training loader details
    Added print statements to log dataset sizes and training loader details.

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ def main():
     print("Preparing dataset.")
         
     dataset = build_dataset(args.dataset, args.root_path, args.shots, preprocess)
-    
+   
+    # Print dataset sizes
+    print(f"Dataset sizes (samples): Train={len(dataset.train_x)}, Val={len(dataset.val)}, Test={len(dataset.test)}")
+
     if args.dataset == 'imagenet':
         val_loader = torch.utils.data.DataLoader(dataset.val, batch_size=256, num_workers=8, shuffle=False, pin_memory=True)
         test_loader = torch.utils.data.DataLoader(dataset.test, batch_size=256, num_workers=8, shuffle=False, pin_memory=True)
@@ -46,7 +49,9 @@ def main():
             train_loader = torch.utils.data.DataLoader(dataset.train_x, batch_size=args.batch_size, num_workers=8, shuffle=True, pin_memory=True)
         else:
             train_loader = build_data_loader(data_source=dataset.train_x, batch_size=args.batch_size, tfm=train_tranform, is_train=True, shuffle=True, num_workers=8)
-
+    # Add training log details
+        print(f"Train Loader Ready: Batch Size={args.batch_size}, Steps per Epoch={len(train_loader)}")
+    
     run_lora(args, clip_model, logit_scale, dataset, train_loader, val_loader, test_loader)
 
 if __name__ == '__main__':
